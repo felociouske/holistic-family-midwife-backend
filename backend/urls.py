d3afe421c4fae72dc6becc63d4c7b4a5f070f225
felociouske <felociouske@gmail.com>
2026-04-09 16:39:30 +0300

    Updated the backend for deployment

--- backend/urls.py
+++ backend/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),  # Admin at root
+    path('admin/', admin.site.urls),  # Also keep /admin for convenience
     path('api/', include('enquiries.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
